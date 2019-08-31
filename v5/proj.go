9dfdeeef9599edcf12d0ac95c7ba0443ecb3c263
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2019-08-31 14:32:20 +0200

    detail

--- v5/proj.go
+++ v5/proj.go
@@ -176,11 +176,11 @@ func (p *PJ) Trans(direction Direction, u1, v1, w1, t1 float64) (u2, v2, w2, t2 
 /*
 Transform a series of coordinates, where the individual coordinate dimension may be represented by a slice that is either
 
- 1. fully populated
+1. fully populated
 
- 2. nil and/or a length of zero, which will be treated as a fully populated slice of zeroes
+2. nil and/or a length of zero, which will be treated as a fully populated slice of zeroes
 
- 3. of length one, i.e. a constant, which will be treated as a fully populated slice of that constant value
+3. of length one, i.e. a constant, which will be treated as a fully populated slice of that constant value
 
 TODO: what if input is constant, but output is not?
 */
